803e7970955cb41eccbfa375c25e6bb0c2db5be3
tatan1121 <jhoanbranth@gmail.com>
2022-07-12 17:01:51 -0500

    Added modal to create new product

--- src/components/admin/Products.vue
+++ src/components/admin/Products.vue
@@ -13,19 +13,72 @@
             :price='item.price'
             :description='item.description'
           />
-          {{productToDelete}}
         </div>
         <div>
           <!-- <b-button v-b-modal.modal-1>Launch demo modal</b-button> -->
           <b-modal
             ref="delete-modal"
-            title="BootstrapVue"
+            title="Advertencia!"
             @ok="confirmDelete()"
             @hidden="resetProductToDelete()"
           >
-            <p class="my-4">Hello from modal!</p>
+            <p class="my-4">¿Está seguro que desea eliminar este producto?</p>
           </b-modal>
         </div>
+        <div>
+          <b-modal
+            id="modal-prevent-closing"
+            ref="newProduct-modal"
+            title="Agrega un nuevo producto"
+            @show="newProductReset"
+            @hidden="newProductReset"
+            @ok="newProduct"
+          >
+            <form ref="form" @submit.stop.prevent="handleSubmit">
+              <b-form-group
+                label="Nombre del producto:"
+                label-for="name-input"
+                invalid-feedback="El nombre es requerido."
+                :state="nameState"
+              >
+                <b-form-input
+                  id="name-input"
+                  v-model="productName"
+                  :state="nameState"
+                  required
+                ></b-form-input>
+              </b-form-group>
+              <b-form-group
+                label="Precio del producto:"
+                label-for="price-input"
+                invalid-feedback="El nombre es requerido."
+                :state="priceState"
+              >
+                <b-form-input
+                  id="price-input"
+                  v-model="productPrice"
+                  :state="priceState"
+                  required
+                ></b-form-input>
+              </b-form-group>
+            </form>
+          </b-modal>
+        </div>
+      </div>
+      <div class="d-flex justify-content-center mt-3">
+        <b-button
+          v-b-modal.modal-prevent-closing
+          variant="primary"
+        >
+          Nuevo producto
+        </b-button>
+        <!-- <button
+          type="button"
+          class="btn btn-primary btn-newproduct d-flex justify-content-center"
+          @click="newProduct"
+          >
+          Nuevo producto
+        </button> -->
       </div>
     </div>
 </template>
@@ -39,6 +92,10 @@ export default {
 
   data() {
     return {
+      productName: '',
+      nameState: null,
+      productPrice: 0,
+      priceState: null,
     };
   },
 
@@ -75,6 +132,15 @@ export default {
     resetProductToDelete() {
       this.deleteProduct('');
     },
+
+    newProduct() {
+
+    },
+
+    newProductReset() {
+
+    },
+
   },
 
   components: {
@@ -92,6 +158,10 @@ export default {
 .card-content {
   margin: 5px 20px;
   width: calc(25% - (20px * 2));
+}
+
+.btn-newproduct {
+  width: 150px;
 }
 
 /* responsive products grid */
